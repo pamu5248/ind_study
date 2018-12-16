fa811df0d878dd4c225ff765938bb6525eb4138d
pamu5248 <38261838+pamu5248@users.noreply.github.com>
2018-12-15 21:53:45 -0700

    Update Fig10.py

--- Fig10/Fig10.py
+++ Fig10/Fig10.py
@@ -1,1 +1,22 @@
+from matplotlib import cm
+import sys
+import os
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import imp
+import scipy.stats
+import pickle
+import math
 
+from wind_tools import geometry as geo
+from wind_tools import plotting as wp
+
+
+from FieldCampaign import read_data_files as rd
+from FieldCampaign import controlRef as cref
+from FieldCampaign import hector_ref as hr
+from FieldCampaign import database as fdb
+from FieldCampaign import models as mod
